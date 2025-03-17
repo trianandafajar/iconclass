d096c66ed31c33d95ebb4e308682bef2029e57d9
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-17 10:30:01 +0700

    Update Mail

--- resources/views/emails/contact_mail.blade.php
+++ resources/views/emails/contact_mail.blade.php
@@ -1,10 +1,13 @@
 @component('mail::message')
-    <h4>Hello Admin, Following contact has messaged you from Contact Page of <a style="font-weight:bold;" href="{{env('APP_URL')}}">{{env('APP_NAME')}}</a></h4>
-    Name: {{ $contactMessage->name }}
-    Email: {{ $contactMessage->emails }}
-    Number: {{ $contactMessage->number }}
-    Message: {{ $contactMessage->message }}
+    <h4>Hello Admin,</h4>
+    <p>Following contact has messaged you from the Contact Page of <a href="{{ env('APP_URL') }}" style="font-weight: bold; text-decoration: none; color: #007bff;">{{ env('APP_NAME') }}</a></p>
 
-    Thanks,
-    {{ config('app.name') }}
+    <p><strong>Name:</strong> {{ $contactMessage->name }}</p>
+    <p><strong>Email:</strong> {{ $contactMessage->emails }}</p>
+    <p><strong>Phone Number:</strong> {{ $contactMessage->number }}</p>
+    <p><strong>Message:</strong> {{ $contactMessage->message }}</p>
+
+    <br>
+    <p>Thanks,<br>
+    {{ config('app.name') }}</p>
 @endcomponent
